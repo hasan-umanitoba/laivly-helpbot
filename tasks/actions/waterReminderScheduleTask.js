3831b97b850f410e11aef0b6fdfb00c765d4b4f1
Hasan Saleem <55816213+hasan-umanitoba@users.noreply.github.com>
2020-04-09 13:23:18 -0500

    fixed water reminder time difference bug

--- tasks/actions/waterReminderScheduleTask.js
+++ tasks/actions/waterReminderScheduleTask.js
@@ -9,8 +9,8 @@ async function exec(app, context, body, payload, respond) {
     var timetoCall = Math.round((newDate.getTime() / 1000));
 
     for (let k = 0; k < timeLength; k++) {
-        timetoCall += intervalTime; // reminds to drink water after every hour 8 times a day
         for (let i = 0; i < totalDayReminders; i++) {
+            timetoCall += intervalTime; // reminds to drink water after every hour 8 times a day
             const result = await app.client.chat.scheduleMessage({
                 token: context.botToken,
                 channel: body.user.id,
